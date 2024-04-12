4168385e9567fa5c8f226720e272c28ff9eee3f4
fomes <fgomesdeluna@gmail.com>
2024-04-12 12:07:18 -0300

    feat: creates static home page

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Slider from "@react-native-community/slider";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./src/assets/logo.png")} style={styles.logo} />
+      <Text style={styles.title}>20 caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor="#ff0000"
+          minimumTrackTintColor="#000"
+          thumbTintColor="#392de9"
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +27,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f3f3ff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  logo: {
+    marginBottom: 30,
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 8,
+  },
+
+  button: {
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
   },
 });
